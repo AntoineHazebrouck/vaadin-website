3cbfca0712f439bdf7cbba58fa1cd90ac0bfd751
Antoine Hazebrouck <antoine.hazebrouck5@gmail.com>
2025-08-21 00:07:12 +0200

    translation fr experiences done

--- src/main/java/antoine/vaadin_website/views/experiences/ExperiencesView.java
+++ src/main/java/antoine/vaadin_website/views/experiences/ExperiencesView.java
@@ -1,20 +1,17 @@
 package antoine.vaadin_website.views.experiences;
 
-import antoine.vaadin_website.components.CustomCard;
 import antoine.vaadin_website.components.Page;
 import antoine.vaadin_website.utils.Responsive;
 import antoine.vaadin_website.views.ContactView;
 import antoine.vaadin_website.views.ProjectsView;
 import antoine.vaadin_website.views.experiences.components.cards.ButInformatiqueCard;
 import antoine.vaadin_website.views.experiences.components.cards.IngenieurInformatiqueCard;
+import antoine.vaadin_website.views.experiences.components.cards.SocgenCard;
 import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.html.H3;
-import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.i18n.LocaleChangeEvent;
 import com.vaadin.flow.i18n.LocaleChangeObserver;
 import com.vaadin.flow.router.Route;
-import java.util.List;
 
 @Route("experiences")
 public class ExperiencesView extends Page implements LocaleChangeObserver {
@@ -35,26 +32,7 @@ public class ExperiencesView extends Page implements LocaleChangeObserver {
 
         var professional = Responsive.column(
             professionalTitle,
-            CustomCard.builder()
-                .width(WIDTH)
-                .minHeight("22.5em")
-                .title("Apprenti ingénieur logiciel")
-                .subtitle("Société Générale - La Défense")
-                .content(
-                    List.of(
-                        new Paragraph(
-                            "Dans un contexte international, j'ai créé, amélioré et maintenu une large chaîne d'outils répondant aux risques structurels et aux besoins de pilotage du groupe vis-à-vis de la direction financière. "
-                        ),
-                        new Paragraph(
-                            "J'ai notamment, au sein de l'équipe data quality, implementé la consolidation, la coherence et la validation qualitative du bilan du groupe. "
-                        ),
-                        new Paragraph(
-                            "J'ai aussi eu l'occasion de mener dans sa totalité un projet de calibration du bilan interne sur un socle agile, priorisant la satisfaction client. "
-                        )
-                    )
-                )
-                .headerSuffix(new Text("2023-2027"))
-                .build()
+            new SocgenCard()
         )
             .alignement(Alignment.CENTER)
             .justify(JustifyContentMode.BETWEEN)
--- src/main/java/antoine/vaadin_website/views/experiences/components/cards/SocgenCard.java
+++ src/main/java/antoine/vaadin_website/views/experiences/components/cards/SocgenCard.java
@@ -4,19 +4,24 @@ import antoine.vaadin_website.components.CustomCard;
 import antoine.vaadin_website.views.experiences.ExperiencesView;
 import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.i18n.LocaleChangeEvent;
 import com.vaadin.flow.i18n.LocaleChangeObserver;
 import java.util.List;
 
-public class IngenieurInformatiqueCard
+public class SocgenCard
     extends Composite<CustomCard>
     implements LocaleChangeObserver {
 
+    Paragraph text1 = new Paragraph();
+    Paragraph text2 = new Paragraph();
+    Paragraph text3 = new Paragraph();
+
     CustomCard card = CustomCard.builder()
         .width(ExperiencesView.WIDTH)
-        .minHeight("15em")
-        .content(List.of())
-        .headerSuffix(new Text("2024-2027"))
+        .minHeight("22.5em")
+        .content(List.of(text1, text2, text3))
+        .headerSuffix(new Text("2023-2027"))
         .build();
 
     @Override
@@ -26,11 +31,11 @@ public class IngenieurInformatiqueCard
 
     @Override
     public void localeChange(LocaleChangeEvent event) {
-        card.setTitle(
-            getTranslation("experiences.ingenieur-informatique.title")
-        );
-        card.setSubtitle(
-            getTranslation("experiences.ingenieur-informatique.subtitle")
-        );
+        card.setTitle(getTranslation("experiences.socgen.title"));
+        card.setSubtitle(getTranslation("experiences.socgen.subtitle"));
+
+        text1.setText(getTranslation("experiences.socgen.text1"));
+        text2.setText(getTranslation("experiences.socgen.text2"));
+        text3.setText(getTranslation("experiences.socgen.text3"));
     }
 }

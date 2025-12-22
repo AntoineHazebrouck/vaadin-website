99412755129ec6f131aea04140e02eabcb0c76b4
Antoine Hazebrouck <antoine.hazebrouck5@gmail.com>
2025-12-22 17:04:50 +0100

    personal info translation

--- src/main/java/antoine/vaadin_website/components/pages/aboutme/PersonalInformation.java
+++ src/main/java/antoine/vaadin_website/components/pages/aboutme/PersonalInformation.java
@@ -5,24 +5,39 @@ import antoine.vaadin_website.utils.Responsive;
 import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.accordion.AccordionPanel;
 import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.i18n.LocaleChangeEvent;
+import com.vaadin.flow.i18n.LocaleChangeObserver;
 
 public class PersonalInformation
     extends Composite<AccordionPanel>
-    implements LayoutMixin {
+    implements LayoutMixin, LocaleChangeObserver {
+
+    private final Span title = new Span("");
+    private final Span phone = new Span("");
+    private final Span email = new Span("");
+    private final Span linkedin = new Span("");
+    private final Span city = new Span("");
+    private final Span drivingLicense = new Span("");
 
     @Override
     protected AccordionPanel initContent() {
         return new AccordionPanel(
-            "Personal information",
-            Responsive.column(
-                new Span("+33 6 51 73 08 05"),
-                new Span("antoine.hazebrouck5@gmail.com"),
-                new Span("LinkedIn TODO"),
-                new Span("59000, Lille"),
-                new Span("French driving license + car")
-            )
+            title,
+            Responsive.column(phone, email, linkedin, city, drivingLicense)
                 .withoutSpacing()
                 .build()
+        );
+    }
+
+    @Override
+    public void localeChange(LocaleChangeEvent event) {
+        title.setText(getTranslation("about.personal-information.title"));
+        phone.setText(getTranslation("about.personal-information.phone"));
+        email.setText(getTranslation("about.personal-information.email"));
+        linkedin.setText(getTranslation("about.personal-information.linkedin"));
+        city.setText(getTranslation("about.personal-information.city"));
+        drivingLicense.setText(
+            getTranslation("about.personal-information.driving-license")
         );
     }
 }

c5c4d04d37f8bf85b6cbae28645e9b994d334b24
Antoine Hazebrouck <antoine.hazebrouck5@gmail.com>
2025-12-22 12:28:31 +0100

    removed tabs

--- src/main/java/antoine/vaadin_website/MainLayout.java
+++ src/main/java/antoine/vaadin_website/MainLayout.java
@@ -1,25 +1,12 @@
 package antoine.vaadin_website;
 
-import antoine.vaadin_website.components.NavBarLink;
-import antoine.vaadin_website.utils.Responsive;
-import antoine.vaadin_website.views.contact.ContactView;
-import antoine.vaadin_website.views.experiences.ExperiencesView;
-import antoine.vaadin_website.views.main.MainView;
-import antoine.vaadin_website.views.projects.ProjectsView;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.orderedlayout.FlexComponent.JustifyContentMode;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.tabs.Tab;
-import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.i18n.LocaleChangeEvent;
 import com.vaadin.flow.i18n.LocaleChangeObserver;
-import com.vaadin.flow.router.AfterNavigationEvent;
-import com.vaadin.flow.router.AfterNavigationObserver;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.Layout;
@@ -31,19 +18,9 @@ import java.util.Locale;
 @PreserveOnRefresh
 public class MainLayout
     extends AppLayout
-    implements
-        AfterNavigationObserver, LocaleChangeObserver, BeforeEnterObserver {
+    implements LocaleChangeObserver, BeforeEnterObserver {
 
     Locale current;
-
-    NavBarLink home = new NavBarLink(VaadinIcon.HOME_O, MainView.class);
-    NavBarLink contact = new NavBarLink(VaadinIcon.CHAT, ContactView.class);
-    NavBarLink experiences = new NavBarLink(
-        VaadinIcon.ACADEMY_CAP,
-        ExperiencesView.class
-    );
-    NavBarLink projects = new NavBarLink(VaadinIcon.CODE, ProjectsView.class);
-    Tabs tabs = tabs();
 
     Button toFrench = new Button("FR", event -> {
         UI.getCurrent().setLocale(Locale.FRENCH);
@@ -55,53 +32,20 @@ public class MainLayout
     });
 
     public MainLayout() {
-        H1 title = new H1("AH");
+        H1 title = new H1("Antoine HAZEBROUCK");
         title
             .getStyle()
-            .set("font-size", "var(--lumo-font-size-l)")
+            .set("font-size", "var(--lumo-font-size-xl)")
             .setMarginLeft("4em")
             .setMarginRight("auto");
 
-        var nav = (HorizontalLayout) Responsive.row(tabs)
-            .justify(JustifyContentMode.CENTER)
-            .wrap()
-            .build();
-
-        nav.getStyle().setMarginTop("auto");
-
-        nav.getStyle().setMarginLeft("auto").setMarginRight("auto");
-
         addToNavbar(title);
-        addToNavbar(true, nav);
 
         toFrench.addClassName("to-french-toggle");
         toEnglish.addClassName("to-english-toggle");
         toEnglish.addClassNames(LumoUtility.Margin.End.SMALL);
 
         addToNavbar(toFrench, toEnglish);
-    }
-
-    private Tabs tabs() {
-        var tabs2 = new Tabs(home, contact, experiences, projects);
-        tabs2.setAutoselect(false);
-        tabs2.addClassName("navbar-tabs");
-        return tabs2;
-    }
-
-    @Override
-    public void afterNavigation(AfterNavigationEvent event) {
-        Tab currentTab =
-            switch (UI.getCurrent().getCurrentView()) {
-                case MainView pass -> home;
-                case ContactView pass -> contact;
-                case ExperiencesView pass -> experiences;
-                case ProjectsView pass -> projects;
-                default -> {
-                    UI.getCurrent().navigate(MainView.class);
-                    yield home;
-                }
-            };
-        tabs.setSelectedTab(currentTab);
     }
 
     @Override
@@ -124,11 +68,6 @@ public class MainLayout
                 UI.getCurrent().setLocale(Locale.ENGLISH);
             }
         }
-
-        home.setText(getTranslation("navigation.home"));
-        contact.setText(getTranslation("navigation.contact"));
-        experiences.setText(getTranslation("navigation.experiences"));
-        projects.setText(getTranslation("navigation.projects"));
 
         current = event.getLocale();
     }

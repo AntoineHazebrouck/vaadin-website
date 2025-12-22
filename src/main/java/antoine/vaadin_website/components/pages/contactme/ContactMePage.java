eb84a980e1d8cd3f3a7b0b94de10020e0176091a
Antoine Hazebrouck <antoine.hazebrouck5@gmail.com>
2025-12-22 17:09:25 +0100

    contact me translation

--- src/main/java/antoine/vaadin_website/components/pages/contactme/ContactMePage.java
+++ src/main/java/antoine/vaadin_website/components/pages/contactme/ContactMePage.java
@@ -3,15 +3,24 @@ package antoine.vaadin_website.components.pages.contactme;
 import antoine.vaadin_website.components.Page;
 import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.Text;
+import com.vaadin.flow.i18n.LocaleChangeEvent;
+import com.vaadin.flow.i18n.LocaleChangeObserver;
 
-public class ContactMePage extends Composite<Page> {
+public class ContactMePage
+    extends Composite<Page>
+    implements LocaleChangeObserver {
+
+    private final Text title = new Text("");
 
     @Override
     protected Page initContent() {
-        var page = new Page()
-            .title(new Text("Contact me"))
-            .body(new ContactForm());
+        var page = new Page().title(title).body(new ContactForm());
 
         return page;
+    }
+
+    @Override
+    public void localeChange(LocaleChangeEvent event) {
+        title.setText(getTranslation("contact.title"));
     }
 }

565934ba5caaa829bb7c8edd2e5edf54b9db637a
Antoine Hazebrouck <antoine.hazebrouck5@gmail.com>
2025-12-22 23:29:50 +0100

    Translation for MegaStorageRemote

--- src/main/java/antoine/vaadin_website/components/pages/projects/MegaStorageRemote.java
+++ src/main/java/antoine/vaadin_website/components/pages/projects/MegaStorageRemote.java
@@ -4,35 +4,48 @@ import antoine.vaadin_website.components.InnerPage;
 import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.i18n.LocaleChangeEvent;
+import com.vaadin.flow.i18n.LocaleChangeObserver;
 
-public class MegaStorageRemote extends Composite<InnerPage> {
+public class MegaStorageRemote
+    extends Composite<InnerPage>
+    implements LocaleChangeObserver {
+
+    private final Text title = new Text("");
+    private final Text subtitle = new Text("");
+
+    private final Text p1 = new Text("");
+    private final Text p2 = new Text("");
+    private final Text p3 = new Text("");
+    private final Text p4 = new Text("");
+    private final Text p5 = new Text("");
 
     @Override
     protected InnerPage initContent() {
         var card = new InnerPage()
-            .title(new Text("Android remote for MegaStorage 300CD"))
-            .subtitle(
-                new Text("Allows me to control my 'jukebox' CD player easily")
-            )
+            .title(title)
+            .subtitle(subtitle)
             .body(
-                new Paragraph(
-                    "This CD player lets you add 300 CDs, yet you need to keep paper notes of which CD is at which position inside e.g. : 'Depeche Mode - Music for the Masses' is at position 23. Paper means you cannot modify it, search for names, and so on."
-                ),
-                new Paragraph(
-                    "I wanted to software-ize this paper list, so I looked through a few ways to control the CD player using code. I had three options : simulating a keyboard via PS/2, using a dedicated data port, or simulating a remote via infrared. The keyboard option was tricky because it is mostly dedicated to entering CD metadata. The data port was too proprietary to figure out (connectics, protocols, ...). So the only option left was infrared, and it turned ou that my Xiaomi phone has an infrared emitter."
-                ),
-                new Paragraph(
-                    "It looks easy on the surface, however infrared handling was a real challenge. There are no standards in infrared, everything is proprierary and this device especially is niche. Finding the proper infrared codes that correspond to the official remote buttons definitly had to be done through retro-engineering."
-                ),
-                new Paragraph(
-                    "To retrieve all the infrared codes, I wired an infrared emitter component to a RaspberryPi and set up the necessary software on the Raspberry. Then I could press the remote's buttons, and the codes would show up in the ssh console. I had to make sure to get the codes in straight 'binary' (up/down times) and reformat them so that I could use them in my own code."
-                ),
-                new Paragraph(
-                    "I managed to turn on and off my CD player through a button in my android app. From that point onward, it was just software implementation. Still, the aim was not to reproduce the remote but to replace the paper list to turn it into a 'Spotify-like' app. I used C# with .NET for Android to build the application to learn the language. The only challenge was the precision of the infrared blasts, espacially with combined instructions. For example, if I want to play 'Depeche Mode - Music for the Masses' which is at position 23, the app actually blasts 5 commands in a sequence : 'Choose disc', 'Two', 'Three', 'Enter', 'Play'. This is tackled by sending the given code 2 to 3 times in a row to make sure, but even with that the precision is not 100%."
-                ),
+                new Paragraph(p1),
+                new Paragraph(p2),
+                new Paragraph(p3),
+                new Paragraph(p4),
+                new Paragraph(p5),
                 new Paragraph()
             );
 
         return card;
+    }
+
+    @Override
+    public void localeChange(LocaleChangeEvent event) {
+        title.setText(getTranslation("projects.megastorage.title"));
+        subtitle.setText(getTranslation("projects.megastorage.subtitle"));
+
+        p1.setText(getTranslation("projects.megastorage.p1"));
+        p2.setText(getTranslation("projects.megastorage.p2"));
+        p3.setText(getTranslation("projects.megastorage.p3"));
+        p4.setText(getTranslation("projects.megastorage.p4"));
+        p5.setText(getTranslation("projects.megastorage.p5"));
     }
 }

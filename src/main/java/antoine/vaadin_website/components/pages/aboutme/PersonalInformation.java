dfe567de617fe568a703cc98a1117485cb367844
Antoine Hazebrouck <antoine.hazebrouck5@gmail.com>
2025-12-22 23:55:20 +0100

    linkedin

--- src/main/java/antoine/vaadin_website/components/pages/aboutme/PersonalInformation.java
+++ src/main/java/antoine/vaadin_website/components/pages/aboutme/PersonalInformation.java
@@ -4,6 +4,7 @@ import antoine.vaadin_website.utils.LayoutMixin;
 import antoine.vaadin_website.utils.Responsive;
 import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.accordion.AccordionPanel;
+import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.i18n.LocaleChangeEvent;
 import com.vaadin.flow.i18n.LocaleChangeObserver;
@@ -15,12 +16,17 @@ public class PersonalInformation
     private final Span title = new Span("");
     private final Span phone = new Span("");
     private final Span email = new Span("");
-    private final Span linkedin = new Span("");
     private final Span city = new Span("");
     private final Span drivingLicense = new Span("");
 
     @Override
     protected AccordionPanel initContent() {
+        var linkedin = new Anchor(
+            "https://www.linkedin.com/in/antoine-hazebrouck-775533261",
+            "LinkedIn"
+        );
+        linkedin.setTarget("_tab");
+
         return new AccordionPanel(
             title,
             Responsive.column(phone, email, linkedin, city, drivingLicense)
@@ -34,7 +40,6 @@ public class PersonalInformation
         title.setText(getTranslation("about.personal-information.title"));
         phone.setText(getTranslation("about.personal-information.phone"));
         email.setText(getTranslation("about.personal-information.email"));
-        linkedin.setText(getTranslation("about.personal-information.linkedin"));
         city.setText(getTranslation("about.personal-information.city"));
         drivingLicense.setText(
             getTranslation("about.personal-information.driving-license")
